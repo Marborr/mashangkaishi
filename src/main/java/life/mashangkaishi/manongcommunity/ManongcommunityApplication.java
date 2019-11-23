8e7bb4cd0b7b194f721697189c471a796d89249b
MayingBo <749286375@qq.com>
2019-11-23 16:52:47 +0800

    mashangkaishi

---
+++ src/main/java/life/mashangkaishi/manongcommunity/ManongcommunityApplication.java
@@ -0,0 +1,13 @@
+package life.mashangkaishi.manongcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManongcommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManongcommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/life/mashangkaishi/manongcommunity/ManongcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package life.mashangkaishi.manongcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManongcommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
