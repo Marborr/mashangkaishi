c1d95344ec22f585e39f6c18ddb7d6498715c8fd
MayingBo <749286375@qq.com>
2020-02-28 21:51:59 +0800

    'a'

--- src/main/java/life/mashangkaishi/manongcommunity/controller/IndexController.java
+++ src/main/java/life/mashangkaishi/manongcommunity/controller/IndexController.java
@@ -1,14 +1,9 @@
 package life.mashangkaishi.manongcommunity.controller;
 
-import life.mashangkaishi.manongcommunity.dto.PageDTO;
 import life.mashangkaishi.manongcommunity.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
@@ -17,17 +12,6 @@ public class IndexController {
 
     @GetMapping("/")
     public String index() {
-
-//        HttpServletRequest request,
-//        Model model,
-//        @RequestParam(name = "page", defaultValue = "1") Integer page,
-//        @RequestParam(name = "size", defaultValue = "5") Integer size,
-//        @RequestParam(name = "search", required = false) String search
-
-//        PageDTO pageDTO = questionService.list(search,page,size);
-//
-//        model.addAttribute("pagination", pageDTO);
-//        model.addAttribute("search",search);
         return "login";
     }
     @GetMapping("/student")
